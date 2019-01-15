4a600306ff2213d4804fc3e854cfefffc2bf57fb
dhyana1984 <xiongyi_1984@foxmail.com>
2019-01-15 23:47:30 +0800

    组件-propd

---
+++ bbs-component/src/PostItem.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+class PostItem extends Component{
+    render(){
+        const {title, author, date} = this.props;
+        return(
+            <li>
+                <div>
+                    {title}
+                </div>
+                <div>
+                    创建人：{author}
+                </div>
+                <div>
+                    创建时间：{date}
+                </div>
+            </li>
+        )
+    }
+   
+}
+
+export default PostItem;
---
+++ bbs-component/src/PostList.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+import PostItem from "./PostItem";
+
+const data = [
+    {title:"大家一起来讨论React吧", author:"张三", date:"2017-09-01 18:00:21"},
+    {title:"前段框架，最爱哪一个", author:"张三", date:"2017-09-01 18:00:21"},
+    {title:"Web App的时代已经到来", author:"张三", date:"2017-09-01 18:00:21"},
+];
+class PostList extends Component{
+    render(){
+        return(
+            <div>
+                帖子列表：
+                <ul>
+                   {
+                       data.map(item => 
+                        <PostItem 
+                            title={item.title}
+                            author={item.author}
+                            date={item.date}
+                            
+                            />)
+                   }
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default PostList;
