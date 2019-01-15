888732aa8270350f0fc716b9e905aa30f42cbdb3
dhyana1984 <xiongyi_1984@foxmail.com>
2019-01-15 23:49:31 +0800

    组件-props

--- bbs-component/src/PostItem.js
+++ bbs-component/src/PostItem.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 
 class PostItem extends Component{
     render(){
+        //props用于父组件把数据传递给子组件
         const {title, author, date} = this.props;
         return(
             <li>
