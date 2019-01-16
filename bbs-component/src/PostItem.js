367abf1be8e3c6b684ce7f5e99033bf1b060905d
dhyana1984 <xiongyi_1984@foxmail.com>
2019-01-17 00:56:34 +0800

    组件-有状态和无状态，属性校验，组件样式

--- bbs-component/src/PostItem.js
+++ bbs-component/src/PostItem.js
@@ -1,53 +1,105 @@
-import React, {Component} from "react";
-
-class PostItem extends Component{
-    constructor(props){
-        //注意一，在构造函数内要先调用super(props), 完成初始化工作
-        super(props);
-        //注意二，定义状态初始化
-        //this.state是唯一改变组件状态的方式，写在构造函数就是定义初始状态
-        this.state={
-            vote:0
-        }
-    }
-    //处理点赞逻辑
-    handleClick(){
-        let vote=this.state.vote;
-        vote++;
-        this.setState({
-            vote:vote
-        });
-    }
-    //组件的state和props都会影响UI，可以把组件看成函数，state和props是输入，UI是输出
-    //UI= Component(props,state)
-    render(){
-        //props用于父组件把数据传递给子组件
-        const {title, author, date} = this.props;
-        return(
-            <li >
-                <div>
-                    {title}
-                </div>
-                <div>
-                    创建人：{author}
-                </div>
-                <div>
-                    创建时间：{date}
-                </div>
-                <div>
-                    <button onClick={() =>{this.handleClick();}}>
-                        点赞
-                    </button>
-                    &nbsp;
-                    <span>
-                        {/* 注意三，调用this.state.vote更新点赞数 */}
-                        {this.state.vote}
-                    </span>
-                </div>
-            </li>
-        )
-    }
+import React, {} from "react";
+import PropTypes from "prop-types";
+//引用针对此组件页面的样式表
+import "./PostItem.css";
+//引用图片
+import likePng from "./images/like-default.png";
+// //有状态PostItem组件
+// class PostItem extends Component{
+//     constructor(props){
+//         //注意一，在构造函数内要先调用super(props), 完成初始化工作
+//         super(props);
+//         //注意二，定义状态初始化
+//         //this.state是唯一改变组件状态的方式，写在构造函数就是定义初始状态
+//         this.state={
+//             vote:0
+//         }
+//     }
+//     //处理点赞逻辑
+//     handleClick(){
+//         let vote=this.state.vote;
+//         vote++;
+//         this.setState({
+//             vote:vote
+//         });
+//     }
+//     //组件的state和props都会影响UI，可以把组件看成函数，state和props是输入，UI是输出
+//     //UI= Component(props,state)
+//     render(){
+//         //props用于父组件把数据传递给子组件
+//         const {title, author, date} = this.props;
+//         return(
+//             <li >
+//                 <div>
+//                     {title}
+//                 </div>
+//                 <div>
+//                     创建人：{author}
+//                 </div>
+//                 <div>
+//                     创建时间：{date}
+//                 </div>
+//                 <div>
+//                     <button onClick={() =>{this.handleClick();}}>
+//                         点赞
+//                     </button>
+//                     &nbsp;
+//                     <span>
+//                         {/* 注意三，调用this.state.vote更新点赞数 */}
+//                         {this.state.vote}
+//                     </span>
+//                 </div>
+//             </li>
+//         )
+//     }
    
+// }
+
+//将PostItem改造为无状态组件
+function PostItem(props){
+    const handleClick= () =>{
+        
+        //PostList定义的handleVote方法和state通过props的onVote属性传过来
+        props.onVote(props.post.id)
+    };
+    const {post}= props;
+    return(
+        <li className="item">
+            <div className="title">
+                {post.title}
+            </div>
+            <div>
+                创建人：<span>{post.author}</span>
+            </div>
+            <div>
+                创建时间：<span>{post.date}</span>
+            </div>
+            <div className="like">
+            {/* <button onClick={handleClick}>
+                         点赞
+                     </button> 
+                     &nbsp;*/}
+                     <span>
+                         <img src={likePng} onClick={handleClick} />
+                     </span>
+                     <span>
+                       
+                         {post.vote}
+                     </span>
+            </div>
+        </li>
+    );
+    
 }
 
+PostItem.prototype={
+    post:PropTypes.shape({
+        id:PropTypes.number,
+        title:PropTypes.string,
+        author:PropTypes.string,
+        date:PropTypes.string,
+        vote:PropTypes.number
+    }).isRequired,
+    onVote:PropTypes.func.isRequired
+}
 export default PostItem;
--- bbs-component/src/PostList.js
+++ bbs-component/src/PostList.js
@@ -1,30 +1,100 @@
 import React, {Component} from "react";
 import PostItem from "./PostItem";
+//引用针对此组件页面的样式表
+import "./PostList.css";
+// const data = [
+//     {title:"大家一起来讨论React吧", author:"张三", date:"2017-09-01 18:00:21"},
+//     {title:"前段框架，最爱哪一个", author:"张三", date:"2017-09-01 18:00:21"},
+//     {title:"Web App的时代已经到来", author:"张三", date:"2017-09-01 18:00:21"},
+// ];
+//无状态组件PostList
+// class PostList extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 帖子列表：
+//                 <ul>
+//                    {
+//                        data.map(item => 
+//                         <PostItem 
+//                             title={item.title}
+//                             author={item.author}
+//                             data={item.date}
+                            
+//                             />)
+//                    }
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
 
-const data = [
-    {title:"大家一起来讨论React吧", author:"张三", date:"2017-09-01 18:00:21"},
-    {title:"前段框架，最爱哪一个", author:"张三", date:"2017-09-01 18:00:21"},
-    {title:"Web App的时代已经到来", author:"张三", date:"2017-09-01 18:00:21"},
-];
+//将PostList设计成有状态组件，负责帖子列表数据的获取以及点赞行为处理
 class PostList extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            posts:[]
+        };
+        this.timer = null //定时器
+        this.handleVote = this.handleVote.bind(this);//ES6 class中，必须手动绑定方法this的指向
+    }
+    //组件挂载阶段，组件被创建，执行初始化，并被挂载到DOM中，完成组件第一次渲染
+    //componentDidMount是挂载阶段被调用第四个方法，只会被调用一次，已经获得DOM结构，依赖DOM节点的操作放到这个方法中
+    //componentDidMount通常用于向服务器端请求数据，调用this.setState会引起组件的重新渲染
+    componentDidMount(){
+        //用setTimeout模拟异步从服务器端获取数据
+        this.timer = setTimeout(() =>{
+            this.setState({
+                posts:[
+                    {id:1,title:"大家一起来讨论React吧", author:"张三", date:"2017-09-01 18:00:33",vote:"0"},
+                    {id:2,title:"前段框架，最爱哪一个", author:"张三", date:"2017-11-03 22:11:26",vote:0},
+                    {id:3,title:"Web App的时代已经到来", author:"张三", date:"2019-03-01 15:20:231",vote:0},
+                ]
+            });
+        },1000)
+    }
+    //组件卸载阶段调用的唯一方法，在组件被卸载前调用，可以执行一些清理工作，比如清楚定时器，清除componentDidMount中手动创建的DOM元素等，避免内存泄漏
+    componentWillUnmount(){
+        if(this.timer){
+            clearTimeout(this.timer);//清除定时器
+        }
+    }
+    handleVote(id){
+        //根据帖子id进行过滤，找到待修改vote属性的帖子，返回新的posts对象
+        const posts = this.state.posts.map(item => {
+            const newItem =item.id ===id?{...item, vote:++item.vote}:item;
+            // if(item.id ===id){
+            //     item.vote+=1;
+            // }
+            // const newItem=item;
+            return newItem;
+        });
+        //修改完state以后需要调用this.setState使用新的posts对象设置state
+        this.setState({
+            posts
+        })
+
+    }
+
     render(){
         return(
-            <div>
-                帖子列表：
+            <div className="container">
+                <h2>话题列表：</h2>
                 <ul>
-                   {
-                       data.map(item => 
-                        <PostItem 
-                            title={item.title}
-                            author={item.author}
-                            data={item.date}
-                            
-                            />)
-                   }
+                    {
+                        this.state.posts.map(item => 
+                            <PostItem 
+                             post={item}
+                             onVote={this.handleVote}
+                             />
+                            )
+                    }
                 </ul>
             </div>
         )
     }
+   
 }
 
 export default PostList;
