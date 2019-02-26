74de25704224195350ea7c3662144193d1f7f380
dhyana1984 <xiongyi_1984@foxmail.com>
2019-02-26 16:48:00 +0800

    修改package.json以build以后直接运行index.html，修改BrowserRouter为HashRouter

--- bbs-mobx/src/components/App/index.js
+++ bbs-mobx/src/components/App/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { inject, observer } from "mobx-react";
 import ModalDialog from "../ModalDialog";
 import asyncComponent from './../../utils/AsyncComponent';
